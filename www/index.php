41abfdd90d881cdd46b50b6598721831f36ec6f5
tobisminer <50511926+tobisminer@users.noreply.github.com>
2022-09-25 00:30:47 +0200

    Fix bug

--- www/index.php
+++ www/index.php
@@ -305,7 +305,7 @@
             <input id="image_button" type="button" class="btn btn-primary" <?php getdisplayStyle('actions', $userLevel); ?> onclick="test()">
          </div>
 		<div class="img-zoom-container">
-		    <img id="sourceImage" src="/media/image.jpg" width=80%>
+		    <img id="sourceImage" src="./media/image.jpg" width=80%>
 		    <div id="resultImage" class="img-zoom-result"></div>
         </div>		
       </div>
